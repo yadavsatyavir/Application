e0e710c1c428a7eefb16077934cc1936726f524d
yadavsatyavir <yadav.satyavir@gmail.com>
2019-07-29 22:52:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def hello_world():
     return "Hello world"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)

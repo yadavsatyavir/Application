0f26e9af58156559f2205cf56cd29da9c6ec43b2
satyavir <yadav.satyavir@gmail.com>
2019-07-29 22:58:00 +0530

    s

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn app:app
+web: gunicorn -b :$PORT app:app
 ps:scale web=1
--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def hello_world():
     return "Hello world"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)

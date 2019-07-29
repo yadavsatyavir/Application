7d336dccfa605a68b7f8d1b90108a5fa1e52054d
satyavir <yadav.satyavir@gmail.com>
2019-07-29 21:58:34 +0530

    as

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 21:57:41 2019
+
+@author: sayadav
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.run()
--- flask_app.py
+++ flask_app.py
@@ -275,5 +275,5 @@ def add_person_name_with_encodding(personName, newface, datafile):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
     #app.run(host='0.0.0.0',port=80)

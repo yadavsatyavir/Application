dfd2aed2abf7620348a17e5866e49a911217e490
yadavsatyavir <yadav.satyavir@gmail.com>
2019-07-30 22:00:41 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "<h1>Hello heroku</h1><p>It is currently </p>"
+    return "<h1>Hello heroku</h1><p>It is currently -UPDATED </p>"
 
 if __name__ == '__main__':
     app.run(debug=True)

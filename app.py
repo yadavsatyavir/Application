e7ec4fd137849b9d426e0e4fd69e24fc5b5b14b1
yadavsatyavir <yadav.satyavir@gmail.com>
2019-07-30 23:08:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return "<h1>Hello heroku</h1><p>It is currently -UPDATED </p>"
+def hello_world():
+    return render_template(
+            "./home.html"  # name of template
+            )
 
 if __name__ == '__main__':
     app.run(debug=True)
